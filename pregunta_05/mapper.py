601389f953e006e893264ba28caa93deebb269f3
DiegoAlexUNALMED <112149672+DiegoAlexUNALMED@users.noreply.github.com>
2023-05-22 19:52:42 -0500

    Update mapper.py

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys 
+if __name__ == "__main__":
+    
+ for line in sys.stdin:
+            numero = line.split('   ')[1]
+            #print(numero)
+            for elemento in numero:
+               mes= line.split('-')[1]
+            #print(mes)
+             
+            sys.stdout.write("{}\t1\n".format(mes))
